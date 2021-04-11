755a4a487451abf4aa3181cb782a934091b75784
Joppe Aarts <joppe@apestaartje.info>
2021-04-11 16:33:58 +0200

    prettier config

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../../public/asset/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../../public/asset/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/component/HelloWorld.vue";
+import HelloWorld from '@/component/HelloWorld.vue';
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+    name: 'Home',
+    components: {
+        HelloWorld,
+    },
 };
 </script>
