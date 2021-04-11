f779cab040e20ee85bebe60930fc70c4c5e77617
Joppe Aarts <joppe@apestaartje.info>
2021-04-11 16:27:28 +0200

    Renamed and moved folders

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <img alt="Vue logo" src="../../public/asset/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import HelloWorld from "@/component/HelloWorld.vue";
 
 export default {
   name: "Home",
